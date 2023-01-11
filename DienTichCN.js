b036d07c0d586f6c88b8d144c7722c885ff54478
Black280197 <121843405+Black280197@users.noreply.github.com>
2023-01-11 11:50:14 +0700

    add Login

--- DienTichCN.js
+++ DienTichCN.js
@@ -1,7 +1,9 @@
-inputWidth = prompt("Nhập chiều dài");
-inputHeight = prompt("Nhập chiều rộng:");
-// let s = inputWidth * inputHeight;
-let cd = parseInt(inputWidth);
-let cr = parseInt(inputHeight);
+ inputWidth = prompt("Nhập chiều dài");
+ inputHeight = prompt("Nhập chiều rộng:");
+ let s = inputWidth * inputHeight;
+let cd = '12' , cr = '12' ;
+ // cd = parseInt(inputWidth);
+ // cr = parseInt(inputHeight);
+console.log(cd,' ',cr);
 // document.write("Dien tich CN =" + s);
-document.write("Dien tich = " + cd*cr);
+// document.write("Dien tich = " + cd*cr);
